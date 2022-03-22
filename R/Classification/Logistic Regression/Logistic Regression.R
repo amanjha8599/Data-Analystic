78afc5b59919594e114323d27794dfc0062de7b6
Aman Jha <amanjha.8599@gmail.com>
2022-03-22 09:14:35 +0530

    classification: - Logistic and KNN classifiers

---
+++ R/Classification/Logistic Regression/Logistic Regression.R
@@ -0,0 +1,33 @@
+dataset = read.csv('Social_Network_Ads.csv')
+dataset$Purchased = factor(dataset$Purchased , c(0,1)) 
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased,SplitRatio = 0.8)
+train_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+train_set[-3] = scale(train_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+model = glm(Purchased ~ ., data = train_set, family = binomial)
+prob_pred = predict(model, test_set[-3], type = 'response')
+pred = ifelse(prob_pred > 0.5, 1,0)
+
+#Plotting the training set with the predictions
+
+library(ElemStatLearn)
+set = train_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(model, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
